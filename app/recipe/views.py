d90ab63d301e1fc3dade86dd2feef84f5505f988
zakeri ,Pooria <pooria.zakeri@eniac-tech.local>
2020-05-27 11:24:39 +0430

    added feature to filter recipes

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -47,9 +47,29 @@ class RecipeViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
+    # defining a common private functions
+
+    def _params_to_ints(self, qs):
+        """ Convert a list of String IDs to a list of integers """
+        return [int(str_id) for str_id in qs.split(',')]
+
+    # http://127.0.0.1:8000/api/recipe/recipes/?ingredients=3&tags=1
     def get_queryset(self):
         """ retrieve the recipes for the authenticated user """
-        return self.queryset.filter(user=self.request.user)
+        tags = self.request.query_params.get('tags')
+        # if no tags provided get function returns None
+        ingredients = self.request.query_params.get('ingredients')
+        queryset = self.queryset
+
+        if tags:
+            tag_ids = self._params_to_ints(tags)
+            queryset = queryset.filter(tags__id__in=tag_ids)
+            # this is the django syntax for filtering on foreignkey objects
+        if ingredients:
+            ingredient_ids = self._params_to_ints(ingredients)
+            queryset = queryset.filter(ingredients__id__in=ingredient_ids)
+
+        return queryset.filter(user=self.request.user)
 
     def get_serializer_class(self):
         """ return appoperiate serializer class """
